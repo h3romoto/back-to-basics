8759a43ceebbd720cebad91ab939cf4c56596c84
Tanaka <t.khondowe@gmail.com>
2022-06-15 08:07:11 -0500

    Queue wrapper class

--- ADS/PythonADS/Data Structures/queue.py
+++ ADS/PythonADS/Data Structures/queue.py
@@ -3,3 +3,16 @@
 # enqueue(), dequeue()
 # Uses the double-ended queue aka Deque provided by Python
 # Deque can add and remove items from both ends of the queue
+
+class Queue():
+  def __init__(self) -> None:
+    self.queue = list()
+
+  def enqueue(self, item):
+    self.queue.append(item)
+
+  def dequeue(self):
+    if (len(self.queue) > 0):
+      self.queue.pop[len(self.queue)-1]
+    else:
+      return None
