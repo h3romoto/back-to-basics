ed5d5b8c2f4d26513b6c4f26c4a1fc19842489ac
Tanaka <t.khondowe@gmail.com>
2022-06-14 22:47:57 -0500

    dfs in py

---
+++ ADS/PythonADS/dfs.py
@@ -0,0 +1,27 @@
+'''
+Depth-First-Searh
+
+Graph as adjacency list implemented using Python dictionary
+'''
+
+graph = {
+    '5' : ['3','7'],
+    '3' : ['2','4'],
+    '7' : ['8'],
+    '2' : [],
+    '4' : ['8'],
+    '8' : []
+}
+
+# set to keep track of visited nodes
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(visited, graph, neighbor)
+
+print("Running DFS...")
+dfs(visited, graph, '5')
