f8bc33dbedaf8f1eda5ba117a7700eaa63272497
Tanaka <t.khondowe@gmail.com>
2022-06-15 07:14:25 -0500

    Queue data structure

---
+++ ADS/PythonADS/Data Structures/queue.py
@@ -0,0 +1,5 @@
+# QUEUES
+# FIFO - First In First Out
+# enqueue(), dequeue()
+# Uses the double-ended queue aka Deque provided by Python
+# Deque can add and remove items from both ends of the queue
